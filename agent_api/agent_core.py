33d6a0d3a5eaf68260a59b80c13643ad3c537864
monkeygeek <monkeygeek@qq.com>
2024-12-07 22:33:36 +0800

    modified:   agent_api/agent_core.py

--- agent_api/agent_core.py
+++ agent_api/agent_core.py
@@ -1,7 +1,9 @@
 from flask import jsonify,Blueprint
+from easyrpa.tools.request_tool import easyrpa_request_wrapper
 
 agent_core_bp =  Blueprint('agent_core',__name__)
 
-@agent_core_bp.route('/api/test', methods=['GET'])
-def test():
-    return jsonify({'message': 'Hello, World!'})
+@agent_core_bp.route('/health/test', methods=['GET'])
+@easyrpa_request_wrapper
+def health_test():
+    return jsonify({'message': 'Healthy!'})
