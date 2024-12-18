8096a9cbcc17f5b427dc5fcca2ed5a9eb94b0072
马浩 <mahao@yunquna.com>
2024-12-18 20:23:59 +0800

    modified:   core/dispatch_task_core.py

--- core/dispatch_task_core.py
+++ core/dispatch_task_core.py
@@ -1,39 +1,37 @@
-from easyrpa.models.easy_rpa_exception import EasyRpaException
-from easyrpa.enums.easy_rpa_exception_code_enum import EasyRpaExceptionCodeEnum
-from easyrpa.tools.apscheduler_tool import APSchedulerTool
-from apscheduler.executors.pool import ThreadPoolExecutor, ProcessPoolExecutor
-from apscheduler.jobstores.memory import MemoryJobStore
 from easyrpa.models.agent_models.heartbeat_check_req_dto import HeartbeatCheckReqDTO
-from easyrpa.tools import request_tool,local_store_tools,machine_tools
+from easyrpa.tools import request_tool,local_store_tools,machine_tools,logs_tool
 from easyrpa.models.agent_models.robot_log_report_req_dto import RobotLogReportReqDTO
 from configuration import app_config
 import requests
 
 def heartbeat_check_handler(params):
     while True:
-        # get url
-        control_url = app_config.EASYRPA_URL
-        api_pash = app_config.HEART_BEAT_CHECK_API
-        url = control_url + api_pash
+        try:
+            # get url
+            control_url = app_config.EASYRPA_URL
+            api_pash = app_config.HEART_BEAT_CHECK_API
+            url = control_url + api_pash
 
-        # build params
-        data = HeartbeatCheckReqDTO(
-            robot_code=build_robot_code(),
-            robot_ip=get_robot_ip(),
-            port= app_config.ROBOT_SERVER_PORT,
-            task_id=local_store_tools.get_data("task_id")
-        )
+            # build params
+            data = HeartbeatCheckReqDTO(
+                robot_code=build_robot_code(),
+                robot_ip=get_robot_ip(),
+                port= app_config.ROBOT_SERVER_PORT,
+                task_id=local_store_tools.get_data("task_id")
+            )
 
-        # report
-        requests.post(url, json=request_tool.request_base_model_json_builder(data))
-        
-        # gren 10-20s
-        import random
-        wait_time = random.uniform(10, 20)
+            # report
+            requests.post(url, json=request_tool.request_base_model_json_builder(data))
+            
+            # gren 10-20s
+            import random
+            wait_time = random.uniform(10, 20)
 
-        # wait 10-20s
-        import time
-        time.sleep(wait_time)
+            # wait 10-20s
+            import time
+            time.sleep(wait_time)
+        except Exception as e:
+            logs_tool.log_business_error(title="heartbeat_check_handler",message="heartbeat error",data=str(e),exc_info=e)
 
 
 def build_robot_code() -> str:
