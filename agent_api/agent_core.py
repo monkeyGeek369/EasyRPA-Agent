4210a517af9ad86cf95803425d635f0635a13f90
monkeygeek <monkeygeek@qq.com>
2024-12-24 23:41:17 +0800

    modified:   agent_api/agent_core.py

--- agent_api/agent_core.py
+++ agent_api/agent_core.py
@@ -1,6 +1,8 @@
 from flask import Blueprint
 from easyrpa.tools.request_tool import easyrpa_request_wrapper
 from easyrpa.tools.json_tools import JsonTool
+from easyrpa.tools import request_tool,local_store_tools,logs_tool
+from core import dispatch_task_core
 
 agent_core_bp =  Blueprint('agent_core',__name__)
 
@@ -8,3 +10,9 @@ agent_core_bp =  Blueprint('agent_core',__name__)
 @easyrpa_request_wrapper
 def health_test(params):
     return JsonTool.any_to_dict({'message': 'Healthy!'})
+
+@agent_core_bp.route('/release/agent', methods=['GET'])
+@easyrpa_request_wrapper
+def release_agent(params):
+    local_store_tools.delete_data("task_id")
+    return JsonTool.any_to_dict({'message': 'Released!'})
