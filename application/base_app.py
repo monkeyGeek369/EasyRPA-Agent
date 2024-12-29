3d3fd878ac58c2c101915d702fad60fbbc842539
monkeygeek <monkeygeek@qq.com>
2024-12-30 00:15:06 +0800

    modified:   application/base_app.py

--- application/base_app.py
+++ application/base_app.py
@@ -2,7 +2,7 @@ from easyrpa.tools.thread_pool_util import ThreadPoolUtil
 
 
 def init_thread_pool():
-    ThreadPoolUtil.init_global_thread_pool("common", max_workers=5, thread_name_prefix='common')
+    ThreadPoolUtil.init_global_thread_pool("common", max_workers=100, thread_name_prefix='common')
 
 def shutdown_thread_pool():
     ThreadPoolUtil.shutdown_global_thread_pool("common")
