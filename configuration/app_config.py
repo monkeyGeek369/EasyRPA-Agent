5610a50920018df16d91b83408417d42bc313c07
monkeygeek369 <monkeygeek@qq.com>
2025-06-17 00:27:05 +0800

    123

--- configuration/app_config.py
+++ configuration/app_config.py
@@ -1,5 +1,5 @@
 #EASYRPA_URL = "http://127.0.0.1:5003"
-EASYRPA_URL = "http://192.168.1.18:5003"
+EASYRPA_URL = "http://192.168.1.17:5003"
 TASK_RESULT_PUSH_API = "/flow/task/result/handler"
 HEART_BEAT_CHECK_API = "/robot/heartbeat"
 ROBOT_LOG_REPORT_API = "/robot/log/report"
